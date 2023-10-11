18ead7563b3b5fd51a5517bd6dffee492e105cf8
punit sharma <punitsharma9034908543@gmail.com>
2023-10-11 12:27:46 +0530

    create jobs

--- src/components/NavBar/Jobs/jobs.jsx
+++ src/components/NavBar/Jobs/jobs.jsx
@@ -1,11 +1,64 @@
-import { NavLink } from "react-router-dom"
+import style from './job.module.css'
+import { Link, NavLink } from "react-router-dom"
 import jobSvg from "./jobs.svg"
+import { HomeProfile } from "../home/HomeProfile"
+import { AddHome } from "../Assets/add-Home"
 export const Jobs = ()=>{
     return(
-        // <div className="job-logo">
-        //     <img src={jobSvg} alt="logo.." />
-        //     <NavLink to="/Jobs">Jobs</NavLink>
-        // </div>
-        <div>jobs</div>
+        <main className={style.container}>
+            <aside>
+                <HomeProfile/>
+            </aside>
+            <aside className={style.midSection}>
+                <section className={style.MidHeading}>
+                        <p style={{fontSize:'20px',fontWeight:'500'}}>Recent job searches</p>
+                        <span style={{fontSize:'20px'}}>Empty</span>
+                </section>
+                <section className={style.MidBody}>
+                    <div style={{marginBottom:'1rem',padding:'0.5rem'}}>
+                    <span style={{fontSize:'20px',fontWeight:'500'}}>Recommended for you</span><br />
+                    <span>Based on your profile</span>
+                    </div>
+                    <div className={style.companyDetails}>
+                        <div className={style.companyImg}>
+                            <img src="https://media.licdn.com/dms/image/C4D0BAQHD_GDvmfGvAA/company-logo_200_200/0/1598067864224?e=1704931200&v=beta&t=BFstj7-navOLUvz66HVWF9bYTyWirQlbJy71KDgkhEQ" alt="" />
+                        </div>
+                        <div>
+                             <Link style={{fontSize:'18px',fontWeight:'500'}}>Frontend Developer</Link><br />
+                             <span style={{fontSize:'14px',fontWeight:'400'}}>Eg Allied Pvt, Ltd.</span><br />
+                             <span style={{fontSize:'13px',fontWeight:'300'}}>India (Remote)</span>
+                            <p style={{fontSize:'13px',fontWeight:'300'}}> 40 minutes ago -<img src="https://play-lh.googleusercontent.com/kMofEFLjobZy_bCuaiDogzBcUT-dz3BBbOrIEjJ-hqOabjK8ieuevGe6wlTD15QzOqw=w240-h480-rw" alt="logo.." height="35px"/> Easy Apply</p>
+                            </div>
+                    </div>
+
+                    <div className={style.companyDetails}>
+                        <div className={style.companyImg}>
+                            <img src="https://media.licdn.com/dms/image/D4D0BAQHeVY-Lx3sxlw/company-logo_100_100/0/1688021947684?e=1704931200&v=beta&t=VZNPngdTmIJGDiC261wpCPPTqVXSYLWIyxXwodBA9Ms" alt="" />
+                        </div>
+                        <div>
+                             <Link style={{fontSize:'18px',fontWeight:'500'}}>Website Developer</Link><br />
+                             <span style={{fontSize:'14px',fontWeight:'400'}}>Shri Modi Group Jaipur</span><br />
+                             <span style={{fontSize:'13px',fontWeight:'300'}}>India (Onsite)</span><br />
+                             <p style={{fontSize:'13px',fontWeight:'300'}}> 2 hours ago -<img src="https://play-lh.googleusercontent.com/kMofEFLjobZy_bCuaiDogzBcUT-dz3BBbOrIEjJ-hqOabjK8ieuevGe6wlTD15QzOqw=w240-h480-rw" alt="logo.." height="35px"/> Easy Apply</p>
+                        </div>
+                    </div>
+
+                    <div className={style.companyDetails}>
+                        <div className={style.companyImg}>
+                            <img src="https://media.licdn.com/dms/image/D4D0BAQG-AvGz1Lbvhw/company-logo_200_200/0/1692726323521?e=1704931200&v=beta&t=FlkG_j--d0TFax_z8oX8v5DhQ5WOy-sLI6wwSnOcbyE" alt="" />
+                        </div>
+                        <div>
+                             <Link style={{fontSize:'18px',fontWeight:'500'}}>Wed Developer</Link><br />
+                              <span style={{fontSize:'14px',fontWeight:'400'}}>Rekruton Technologies</span><br />
+                             <span style={{fontSize:'13px',fontWeight:'300'}}>India (Remote)</span>
+                             <p style={{fontSize:'13px',fontWeight:'300'}}> 4 hours ago -<img src="https://play-lh.googleusercontent.com/kMofEFLjobZy_bCuaiDogzBcUT-dz3BBbOrIEjJ-hqOabjK8ieuevGe6wlTD15QzOqw=w240-h480-rw" alt="logo.." height="35px"/> Easy Apply</p>
+                        </div>
+                    </div>
+                </section>
+            </aside>
+            <aside>
+                <AddHome/>
+            </aside>
+        </main>
     )
 }
--- src/components/NavBar/notification/notification.jsx
+++ src/components/NavBar/notification/notification.jsx
@@ -1,11 +1,56 @@
-import { NavLink } from "react-router-dom"
+import { Link, NavLink, useNavigate } from "react-router-dom"
 import notificationSvg from "./notification.svg"
+import { AddHome } from "../Assets/add-Home"
+import style from './navbar.module.css'
+
 export const Notification = ()=>{
+    const navigate = useNavigate();
     return(
-        // <div className="notification-logo">
-        //     <img src={notificationSvg} alt="logo.." />
-        //     <NavLink to="/notification">Notification</NavLink>
-        // </div>
-        <div>notification</div>
+        <main className={style.container}>
+            <section className={style.sideLeft}>
+                <span style={{fontWeight:'400', fontSize:'18px'}}>Manage your <br /> account</span><br />
+                <br />
+                <Link>View Account</Link>
+            </section>
+            <div>
+                 <section className={style.heading}>
+                     <p>Notifications</p>
+                 </section>
+                 <section className={style.NotificationContainer}>
+                    <ul>
+                        <li>
+                            <span>Talking facts during a managerial job interview can work wonders,
+                                 says this career coach.</span>
+                        </li>
+                        <hr />
+                        <li>
+                            <span>
+                                Web Development -opportunities in India
+                            </span><br />
+                            <button onClick={()=>navigate('/jobs')}>View jobs</button>
+                        </li>
+                        <hr />
+                        <li>
+                            <span>
+                                Your Wednesday Daily Rundown. eV firms spark campus hiring;<br />
+                                Indian It eyes new markets; and other top news for you
+                            </span>
+                        </li>
+                        <hr />
+                        <li>
+                            <span>
+                                Anubudh is hiring: FullStack Intern. See this and 4 other job recommendations
+                            </span><br />
+                            <button onClick={()=>navigate('/jobs')}>View Jobs</button>
+                        </li>
+                        <hr />
+                    </ul>
+                 </section>
+            </div>
+            
+            <section>
+                <AddHome/>
+            </section>
+        </main>
     )
 }
