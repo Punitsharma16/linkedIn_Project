deb14ee11e09a324b571ca654d395738e0a0ca3b
punit sharma <punitsharma9034908543@gmail.com>
2023-10-18 12:18:06 +0530

    new

--- src/components/Login/Login-Navbar.jsx
+++ src/components/Login/Login-Navbar.jsx
@@ -9,6 +9,7 @@ export const Navbar = ()=>{
                 <h3>Linked </h3>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt="linkedin-logo" height="35px"/>
             </div>
+            
             <button onClick={()=>navigate('/signup')}>Join Now</button>
         </main>
     )
